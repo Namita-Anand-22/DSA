f6eeb2f3ecd27db012f028ddee73cb0746a69a1d
Namita-Anand-22 <119647367+Namita-Anand-22@users.noreply.github.com>
2025-01-31 01:34:36 +0530

    Create remove_element.py

---
+++ remove_element.py
@@ -0,0 +1,10 @@
+# 27. Remove Element: https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def removeElement(self, nums, val):
+        i = 0 # slow pointer
+        for j in range(0, len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i+=1
+        return i
